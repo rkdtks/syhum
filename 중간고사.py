458a44dc99085cf561832bb910431fb70b280822
rkdtks <rkdtks86821@naver.com>
2021-04-26 01:04:52 +0900

    중간고사

---
+++ 중간고사.py
@@ -0,0 +1,9 @@
+##중간고사
+print("갤러고 게임시작")
+print("적비행기 발생")
+enermy=['1.발사',"2.오른쪽이동","3.왼쪽이동"]
+print(enermy[0:3])
+enermy1={1:"총알발사",2:"회피",3:"회피"}
+
+enermy2=enermy1[int(input("입력하세요"))]
+print(enermy2)
